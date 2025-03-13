b922cfb59c9b09fb96f7e6dbb32e1629d749ae73
arm-out <arminsuraj@gmail.com>
2025-03-13 17:20:27 +1100

    Dracula gtk theme

--- home.nix
+++ home.nix
@@ -15,6 +15,18 @@
     userEmail = "arminsuraj@gmail.com";
   };
 
+  # GTK Theme
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Dracula";
+      package = pkgs.dracula-theme;
+    };
+  };
+
+  home.sessionVariables = {
+    GTK_THEME = "Dracula";
+  };
 
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
--- programs/default.nix
+++ programs/default.nix
@@ -14,5 +14,8 @@
 
     # Second brain
     obsidian
+
+    # GTK Theme
+    dracula-theme
   ];
 }
