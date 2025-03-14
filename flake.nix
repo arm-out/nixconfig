3e77a423ca8beabcefe36da1f9d972e538676368
arm-out <arminsuraj@gmail.com>
2025-03-14 15:35:37 +1100

    broken hypridle

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
 
 	{
 	   programs.hyprland.enable = true;
+           programs.hyprland.withUWSM = true;
 	   environment.sessionVariables = {
 	     NIXOS_OZONE_WL = "1";    # Hint electron apps to use Wayland
 	   };
--- programs/desktop/default.nix
+++ programs/desktop/default.nix
@@ -1,10 +1,15 @@
 { pkgs, config, ... }:
 
 {
-  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+  home.file = {
+    ".config/hypr/hyprland.conf".source = ./hyprland.conf;
+    ".config/hypr/hypridle.conf".source = ./hypridle.conf;
+  };
 
   home.packages = with pkgs; [
     wofi
     waybar
+    hypridle
   ];
+
 }
